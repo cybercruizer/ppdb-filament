d4515352d4eb77581341557bfdef0b17ee8c745d
A Nurkholis <cybercruizer@gmail.com>
2025-09-29 17:34:43 +0700

    nullable slug

--- database/migrations/2025_09_29_165312_add_slug_to_posts.php
+++ database/migrations/2025_09_29_165312_add_slug_to_posts.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('slug')->after('title')->unique();
+            $table->string('slug')->after('title')->nullable();
         });
     }
 
