a9a8c858bd7c764f69abdeccb4a9097c5027ee17
A Nurkholis <cybercruizer@gmail.com>
2025-09-30 16:35:30 +0700

    siswa-murid

--- resources/views/livewire/create-pendaftar.blade.php
+++ resources/views/livewire/create-pendaftar.blade.php
@@ -8,10 +8,10 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                     </svg>
-                    Daftar Siswa Baru
+                    Daftar Calon Murid Baru
                 </h1>
                 <p class="text-blue-100 text-lg mt-2"> <!-- Larger text with better contrast -->
-                    Silakan isi formulir di bawah untuk mendaftar sebagai calon siswa baru.
+                    Silakan isi formulir di bawah untuk mendaftar sebagai calon murid baru.
                 </p>
             </div>
             
